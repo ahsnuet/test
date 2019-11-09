539f9e90641a9d195596f0a0835cc396e0eed56a
ahsan <ahsan@enginesound.cn>
2019-11-09 09:45:59 +0800

    Minimum Window Size has been set

--- Alpha/main.cpp
+++ Alpha/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("First App");
+    w.setMinimumSize(500,500);
     w.show();
 
     return a.exec();
